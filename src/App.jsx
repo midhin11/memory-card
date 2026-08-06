97919518953c206f90a707ac850f1930f8e93fa1
midhin11 <midhincr7@gmail.com>
2026-08-06 05:43:16 +0530

    Dummy header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 // import { useState } from 'react'
+// import { Header } from
 
 import './App.css'
 
 export default function App() {
-  
+  return(
+    <Header/>
+  )
+}
+
+function Header() {
+  return (
+    <div>
+      <div>memory<span>.</span>match</div>
+      <div>New round ready</div>
+    </div>
+  )
 }
