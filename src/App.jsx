fb1daf6cbba3bf620eae57fd4e271712b3dce768
midhin11 <midhincr7@gmail.com>
2026-08-07 05:44:04 +0530

    Create hero and header and add styles
    Created hero and header sections for the page and added styles to them

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,44 @@ import './App.css'
 
 export default function App() {
   return(
-    <Header/>
+    <div className="game-container">
+      <Header/>
+      <Hero/>
+    </div>
   )
 }
 
 function Header() {
   return (
-    <div>
-      <div>memory<span>.</span>match</div>
-      <div>New round ready</div>
-    </div>
+    <header>
+      <div className='brand'>
+        <span className='brand-logo'>✦</span>
+        memory<span className='brand-dot'>.</span>match
+        </div>
+      <div className='label'><span className='status'></span>New round ready</div>
+    </header>
+  )
+}
+
+function Hero() {
+  return(
+    <section className="hero">
+      <div className='hero-text'>
+        <p className='recall'>a little game of recall</p>
+        <h1>Keep your<br/><span>head in the game.</span></h1>
+        <p className='intro'>Choose a card, watch the board shift, and see how far your memory can take you.</p>
+      </div>
+      <div className='score-card'>
+        <div className='score-item'>
+          <div>Current Score</div>
+          <strong>00</strong>
+        </div>
+        <div className="score-seperator"></div>
+        <div className='score-item'>
+          <div>Best Score</div>
+          <strong>00</strong>
+        </div>
+      </div>
+    </section>
   )
 }
