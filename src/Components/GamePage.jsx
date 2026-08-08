d3d93bc233a8d23d30006905bb8d8bf87295ebc8
midhin11 <midhincr7@gmail.com>
2026-08-08 11:27:01 +0530

    Create footer section
    Created a footer section with credits and hints. Also added in github
    and linkedin redirect links.

--- src/Components/GamePage.jsx
+++ src/Components/GamePage.jsx
@@ -9,12 +9,21 @@ export default function GamePage() {
   )
 }
 
-function Footer() {
-  
-}
-
 function GameArea() {
 
+}
+
+function Footer() {
+  return (
+    <section className="footer">
+      <div className="footer-ins">Every click reshuffles the board</div>
+      <div className="footer-credit">Built for a sharp mind <b>♥</b></div>
+      <div className="footer-links">
+        <a href="https://github.com/lidge-jun" target="blank" rel="noreferrer">Github <span>↗</span></a>
+        <a href="https://www.linkedin.com/in/williamhgates/" target="blank" rel="noreferrer">Linkedin <span>↗</span></a>
+      </div>
+    </section>
+  )
 }
 
 function Header() {
